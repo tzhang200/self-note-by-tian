3f02d447ca60d69de502c0cdc0665ec578fbcae6
T Zhang <tzhang200@gmail.com>
2015-05-26 21:57:49 -0700

    Initial commit

---
+++ app/database/migrations/2015_05_27_043132_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        /*
+		Schema::table('users', function(Blueprint $table)
+		{
+			//
+		});
+        */
+        Schema::create('users', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('password', 80);
+            $table->boolean('confirmed')->default(0);
+            $table->string('confirmcode')->nullable();
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        /*
+		Schema::table('users', function(Blueprint $table)
+		{
+			//
+		});
+        */
+        Schema::drop('users');
+	}
+
+}
